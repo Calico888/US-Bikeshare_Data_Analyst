a974f33a8a9bab654247588a814dbf18055ecd87
Philipp Keupp <escudokoala@gmail.com>
2018-07-26 18:30:21 +0200

    Created and finished method (get_filters)

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -17,13 +17,28 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
-
+    city = input('Please enter a city (Chicago, New York, Washington):')
+    while True:
+        city = input ('Please enter a city (Chicago, New York, Washington)')
+        if city.lower() not in ('chicago','new york','washington'):
+            print('Invalid Input')
+        else:
+            break
     # get user input for month (all, january, february, ... , june)
-
+    while True:
+        month = input ('Please enter a month (all, january, february, ... , june)')
+        if month.lower() not in ('all','january','february', 'march', 'april','may','june'):
+            print('Invalid Input')
+        else:
+            break
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
-
+    while True:
+        day = input ('Please enter a day of week (all, monday, tuesday, ... sunday)')
+        if day.lower() not in ('all','monday','tuesday', 'wednesday', 'thursday','friday','saturday', 'sunday'):
+            print('Invalid Input')
+        else:
+            break
 
     print('-'*40)
     return city, month, day
